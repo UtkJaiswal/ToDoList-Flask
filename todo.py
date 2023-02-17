2c773be23db4a38a836d4cbcef66243419f99f17
utkarsh <utkjaiswal58@gmail.com>
2023-02-17 13:26:33 +0530

    repeated code removed

--- todo.py
+++ todo.py
@@ -9,10 +9,6 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/tasks'
 mongo = PyMongo(app)
 
 
-from flask import Flask, jsonify, request
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-
 app = Flask(__name__)
 
 # Configure the app with MongoDB
