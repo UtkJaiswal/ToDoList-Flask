19f72e48b4eb5c455d5d1eb8fbaf867944e31feb
utkarsh <utkjaiswal58@gmail.com>
2023-02-17 13:24:07 +0530

    Test API added

--- todo.py
+++ todo.py
@@ -9,5 +9,24 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/tasks'
 mongo = PyMongo(app)
 
 
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+# Configure the app with MongoDB
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/tasks'
+mongo = PyMongo(app)
+
+
+#test api
+@app.route('/', methods=['GET'])
+def test_API():
+    return jsonify({'message': "Your API is working fine"})
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
