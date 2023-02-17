356f28033c35ab2c34341f75d798fdd8ac544133
utkarsh <utkjaiswal58@gmail.com>
2023-02-17 13:08:32 +0530

    Flask setup and mongoDB connected

---
+++ todo.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+# Configure the app with MongoDB
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/tasks'
+mongo = PyMongo(app)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
